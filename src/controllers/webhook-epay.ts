b3f432b8937072024ad392bd608bfd966c9503c8
PortWatcher <jury@skyquakers.com>
2023-10-29 22:07:25 +0900

    fix: handle epay sometimes callbackurl query got trimmed

--- src/controllers/webhook-epay.ts
+++ src/controllers/webhook-epay.ts
@@ -15,45 +15,52 @@ export const onEPayEvent = async function (req: Request, res: Response, next: Ne
   try {
     const { type, pid, out_trade_no, money, trade_no, name: title, sign: serverSign, trade_status, param } = req.query
 
-    const unescaped = decodeURIComponent(param as string).replaceAll('\\&quot;', '"')
-    const meta = JSON.parse(unescaped) as EPayMetaParams
-
-    const md5Reg = /^[a-f0-9]{32}$/gi
-
-    if (!md5Reg.test(serverSign as string)) {
-      return res.status(401).json('sign check error')
-    }
-
-    if (res.locals.epays) {
-      let verified = false
-
-      const data: PresignedEPayPaymentParams = {
-        pid: Number(pid),
-        out_trade_no: out_trade_no as string,
-        notify_url: meta.notify_url,
-        return_url: meta.return_url,
-        name: title as string,
-        money: Number(money).toFixed(2),
-        clientip: meta.clientip,
-        device: 'pc',
-        sign_type: 'MD5',
-        type: type as EPayType,
-        param: param as string,
+    const paramString = param as string
+    if (paramString.slice(-1) !== encodeURIComponent('}')) {
+      if (!dangerouslySkipVerify) {
+        return res.status(401).json('sign check error')
       }
-
-      for (const epay of res.locals.epays.values()) {
-        const clientSign = sign(data, epay.key)
-        console.log(`clientSign for ${epay.endpoint}`, clientSign)
-        if (clientSign === serverSign) {
-          verified = true
-          break
+    } else {
+      const unescaped = decodeURIComponent(param as string).replaceAll('\\&quot;', '"').replace('\\&', '"')
+      const meta = JSON.parse(unescaped) as EPayMetaParams
+  
+      const md5Reg = /^[a-f0-9]{32}$/gi
+  
+      if (!md5Reg.test(serverSign as string)) {
+        return res.status(401).json('sign check error')
+      }
+  
+      if (res.locals.epays) {
+        let verified = false
+  
+        const data: PresignedEPayPaymentParams = {
+          pid: Number(pid),
+          out_trade_no: out_trade_no as string,
+          notify_url: meta.notify_url,
+          return_url: meta.return_url,
+          name: title as string,
+          money: Number(money).toFixed(2),
+          clientip: meta.clientip,
+          device: 'pc',
+          sign_type: 'MD5',
+          type: type as EPayType,
+          param: param as string,
         }
-      }
-
-      if (!verified) {
-        console.log('serverSign', serverSign)
-        if (!dangerouslySkipVerify) {
-          return res.status(401).json('sign check error')
+  
+        for (const epay of res.locals.epays.values()) {
+          const clientSign = sign(data, epay.key)
+          console.log(`clientSign for ${epay.endpoint}`, clientSign)
+          if (clientSign === serverSign) {
+            verified = true
+            break
+          }
+        }
+  
+        if (!verified) {
+          console.log('serverSign', serverSign)
+          if (!dangerouslySkipVerify) {
+            return res.status(401).json('sign check error')
+          }
         }
       }
     }
