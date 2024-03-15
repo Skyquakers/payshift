df19bf030d6df1841b06daf02a9ba4763f143398
PortWatcher <root@pwhack.me>
2024-03-15 20:24:14 +0900

    chore: alipay test transfer api

--- src/providers/alipay.test.ts
+++ src/providers/alipay.test.ts
@@ -1,18 +1,20 @@
-import { expect } from "vitest"
+import { expect, describe, it } from "vitest"
 import { AlipayProvider, CurrencyCode, Payshift } from "../index"
 import path from 'path'
 import { appId } from "../../certs/alipay/secret"
+import { testReceiver } from "../../certs/alipay/secret"
 
 export { appId }
-export const privateKeyPath = path.join(__filename, '../../certs/alipay/private-key.pem')
-export const alipayPublicKeyPath = path.join(__filename, '../../certs/alipay/alipay-public-key.crt')
+export const privateKeyPath = path.join(__filename, '../../../certs/alipay/private-key.pem')
+export const alipayPublicKeyPath = path.join(__filename, '../../../certs/alipay/alipay-public-key.crt')
 
 const provider = new AlipayProvider(appId, privateKeyPath, alipayPublicKeyPath, 'http://www.taobao.com')
 const payshift = new Payshift([provider])
 
 describe('AlipayProvider', function () {
+  const out_trade_no = 'ALIPfdf1211sdfsd12gfddsgs3'
+
   it('desktop payment link should be a string', async function () {
-    const out_trade_no = 'ALIPfdf1211sdfsd12gfddsgs3'
     const subject = 'abc'
     const body = '234'
     const total_amount = 1
@@ -76,5 +78,22 @@ describe('AlipayProvider', function () {
     }
     const ok = provider.sdk.checkNotifySign(postData)
     expect(ok).to.be.true
+  })
+
+  it('check transfer', async function () {
+    const result = await provider.transfer({
+      amountYuan: 0.1,
+      outTradeNo: out_trade_no,
+      receiver: {
+        type: 'ALIPAY_LOGON_ID',
+        id: testReceiver.id,
+        name: testReceiver.realName,
+      },
+      title: 'test transfer',
+    })
+
+    console.log(result)
+
+    expect(result.code).to.equal('10000')
   })
 })
