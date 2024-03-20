a095462f8682214a0597ad13ce577a08a7d00bc6
PortWatcher <root@pwhack.me>
2024-03-20 14:34:55 +0800

    feat: support NewRenewalSuccess for ccbill

--- src/controllers/webhook-ccbill.test.ts
+++ src/controllers/webhook-ccbill.test.ts
@@ -8,19 +8,15 @@ const payshift = new Payshift([ccbill])
 payshift.startWebServer('http://localhost:3000', 3000)
 
 describe('CCBill Webhooks', async function () {
-  let res
-
-  beforeAll(async function () {
-    res = await fetch('http://localhost:3000/webhooks/ccbill?eventType=NewSaleSuccess', {
+  test('NewSaleSuccess should return OK', async function () {
+    const res = await fetch('http://localhost:3000/webhooks/ccbill?eventType=NewSaleSuccess', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(testCCBillWebhookEvent),
     })
-  })
 
-  test('should return OK', async function () {
     expect(res.status).toBe(200)
     expect(await res.text()).toBe('OK')
   })
--- src/controllers/webhook-ccbill.ts
+++ src/controllers/webhook-ccbill.ts
@@ -4,7 +4,7 @@ import { converNumberToCurrencyCode } from "../utils"
 import { CurrencyCode } from "../currency"
 
 
-type CCBillEventType = 'NewSaleSuccess'
+type CCBillEventType = 'NewSaleSuccess' | 'RenewalSuccess'
 
 export const onCCBillEvent = async function (req: Request, res: Response, next: NextFunction) {
   console.log('[payshift]: onCCBillEvent')
@@ -19,38 +19,49 @@ export const onCCBillEvent = async function (req: Request, res: Response, next: 
     if (!ccbill) {
       throw new Error('ccbill provider not configured')
     }
-    const {
-      transactionId, dynamicPricingValidationDigest,
-      billedCurrencyCode, billedInitialPrice, title,
-      initialPeriod, subscriptionInitialPrice, subscriptionCurrencyCode,
-      recurringPeriod, subscriptionRecurringPrice
-    } = req.body
-    const hash = ccbill.generateDigest(
-      subscriptionInitialPrice,
-      initialPeriod,
-      subscriptionCurrencyCode,
-      ccbill.salt,
-      subscriptionRecurringPrice,
-      recurringPeriod,
-    )
-
-    if (hash !== dynamicPricingValidationDigest) {
-      throw new Error(`dynamicPricingValidationDigest mismatched, expect ${hash}, got ${dynamicPricingValidationDigest}`)
-    }
-
-    const currency = converNumberToCurrencyCode(Number(billedCurrencyCode))
 
     const eventType = req.query.eventType as CCBillEventType
+    const { transactionId, billedCurrencyCode } = req.body
+    const currency = converNumberToCurrencyCode(Number(billedCurrencyCode))
 
     if (eventType === 'NewSaleSuccess') {
+      const {
+        transactionId, dynamicPricingValidationDigest,
+        billedInitialPrice,
+        initialPeriod, subscriptionInitialPrice, subscriptionCurrencyCode,
+        recurringPeriod, subscriptionRecurringPrice
+      } = req.body
+      const hash = ccbill.generateDigest(
+        subscriptionInitialPrice,
+        initialPeriod,
+        subscriptionCurrencyCode,
+        ccbill.salt,
+        subscriptionRecurringPrice,
+        recurringPeriod,
+      )
+  
+      if (hash !== dynamicPricingValidationDigest) {
+        throw new Error(`dynamicPricingValidationDigest mismatched, expect ${hash}, got ${dynamicPricingValidationDigest}`)
+      }
+  
       await trigger('charge.succeeded', {
         amount: currency === CurrencyCode.JPY ? Math.round(Number(billedInitialPrice)) : Math.round(Number(billedInitialPrice) * 100),
         tradeNo: transactionId,
         outTradeNo: req.body['X-outTradeNo'],
-        title,
         currency,
         provider: 'ccbill',
         name: 'charge.succeeded',
+      })
+    } else if (eventType === 'RenewalSuccess') {
+      console.log(req.body)
+      const { billedAmount } = req.body
+
+      await trigger('invoice.paid', {
+        provider: 'ccbill',
+        name: 'invoice.paid',
+        tradeNo: transactionId,
+        amount: currency === CurrencyCode.JPY ? Math.round(Number(billedAmount)) : Math.round(Number(billedAmount) * 100),
+        currency,
       })
     }
     return res.status(200).send('OK')
